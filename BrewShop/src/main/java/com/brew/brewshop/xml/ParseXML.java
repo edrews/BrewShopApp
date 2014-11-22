9b1252931af8b18c3db4cf0b0e74d362e782930d
DougEdey <Doug.edey@gmail.com>
2014-11-22 11:54:35 -0500

    Add first pass of the ParseXML Handler. Just supports BeerXML for now.

---
+++ BrewShop/src/main/java/com/brew/brewshop/xml/ParseXML.java
@@ -0,0 +1,88 @@
+package com.brew.brewshop.xml;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import com.brew.brewshop.storage.recipes.Recipe;
+import com.brew.brewshop.xml.BeerXML;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Doug Edey on 21/11/14.
+ */
+public class ParseXML {
+    public static Recipe[] readRecipe(String filename) {
+        File recipeFile = new File(filename);
+
+        String type = checkFileType(recipeFile);
+        if (type.equals("beerxml")) {
+             return BeerXML.readFile(recipeFile);
+        }
+
+        return null;
+    }
+
+    public static Recipe[] readRecipe(InputStream inputStream, String type) {
+        String recipeData = "";
+
+        if (type.equals("beerxml")) {
+            return BeerXML.readInputStream(inputStream);
+        }
+
+        return null;
+    }
+
+    public static String checkRecipeType(String buffer) {
+        if (buffer.indexOf("BeerXML Format") > -1 || buffer.indexOf("<RECIPES>") > -1 )
+            return "beerxml";
+        if (buffer.indexOf("STRANGEBREWRECIPE") > -1)
+            return "sb";
+        if ((buffer.indexOf("generator=\"qbrew\"") > -1) || (buffer.indexOf("application=\"qbrew\"") > -1))
+            return "qbrew";
+
+        return null;
+    }
+
+    private static String checkFileType(File recipeFile) {
+
+        if (recipeFile.getAbsolutePath().endsWith(".rec"))
+            return "promash";
+
+        // let's open it up and have a peek
+        // we'll only read 10 lines
+
+        if (recipeFile.getAbsolutePath().endsWith(".qbrew") || (recipeFile.getAbsolutePath().endsWith(".xml"))) {
+            try {
+                FileReader in = new FileReader(recipeFile);
+                BufferedReader inb = new BufferedReader(in);
+                String c;
+                int i = 0;
+                while ((c = inb.readLine()) != null && i < 10) {
+                    // check for an opening tag of Recipes too
+                    if (c.indexOf("BeerXML Format") > -1 || c.indexOf("<RECIPES>") > -1 )
+                        return "beerxml";
+                    if (c.indexOf("STRANGEBREWRECIPE") > -1)
+                        return "sb";
+                    if ((c.indexOf("generator=\"qbrew\"") > -1) || (c.indexOf("application=\"qbrew\"") > -1))
+                        return "qbrew";
+                    i++;
+                }
+            } catch (Exception e) {
+                Log.e("BrewShop", "Error opening XML File", e);
+            }
+        }
+        return "";
+    }
+
+
+}
