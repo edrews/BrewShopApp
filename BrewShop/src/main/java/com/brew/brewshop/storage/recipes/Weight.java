737789c30af3a36d00a0e86ac4980458ffec2656
DougEdey <Doug.edey@gmail.com>
2014-11-22 11:57:38 -0500

    Add helper to set a weight by string input

--- BrewShop/src/main/java/com/brew/brewshop/storage/recipes/Weight.java
+++ BrewShop/src/main/java/com/brew/brewshop/storage/recipes/Weight.java
@@ -3,8 +3,11 @@ package com.brew.brewshop.storage.recipes;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.IllegalFormatException;
+
 public class Weight implements Parcelable {
     private double ounces;
+    private Quantity quantity;
 
     public Weight() { }
 
@@ -52,4 +55,20 @@ public class Weight implements Parcelable {
             return new Weight[size];
         }
     };
+
+    public Weight(String inWeight) throws Exception {
+        String sections[] = inWeight.trim().split(" ");
+        if (sections.length != 2) {
+            throw new Exception("String for the weight doesn't match \"<number> <unit>\"");
+        }
+
+        double size = Double.parseDouble(sections[0]);
+        String unit = sections[1];
+        quantity = new Quantity();
+        quantity.setUnits(unit);
+        quantity.setAmount(size);
+
+        // For now, set the ounces as the default value.
+        ounces = quantity.getValueAs(Quantity.OUNCES);
+    }
 }
